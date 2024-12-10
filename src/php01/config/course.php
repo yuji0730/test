26502cf257f01823f4512bf96e66bfc35b34f81c
yuji <pgyuji0730@gmail.com>
2024-12-11 08:46:19 +0900

    リモートリポジトリの変更

---
+++ src/php01/config/course.php
@@ -0,0 +1,3 @@
+<?php
+$company = htmlspecialchars($_POST['company'], ENT_QUOTES);
+print "会社名は" . $company . "ですね";
---
+++ src/php01/index03.php
@@ -0,0 +1,10 @@
+<?php
+$name1 = "Tanaka";
+$last_name = "Yamada";
+$first_name = "Saburo";
+
+echo $name1 . "Jiro";
+echo "<br />";
+echo $last_name . $first_name;
+
+
